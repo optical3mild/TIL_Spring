d40eca59a741545807ad7503c4ffe9811b573cf4
optical3mild <chupa.pal.chad@gmail.com>
2019-10-05 00:52:01 +0900

    lec12 : chart - JFreeChart

--- spring02/src/main/java/com/example/spring02/controller/chart/JFreeChartController.java
+++ spring02/src/main/java/com/example/spring02/controller/chart/JFreeChartController.java
@@ -1,21 +1,72 @@
 package com.example.spring02.controller.chart;
 
+import java.io.FileOutputStream;
+
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.spring02.service.chart.JFreeChartService;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
 
 // controller bean으로 등록
 @Controller
 // 공통적인 url mapping
-@RequestMapping("chart/*")
+@RequestMapping("jchart/*")
 public class JFreeChartController {
+	
+	@Inject
+	JFreeChartService chartService;
 	
 	// 세부적인 url mapping
 	@RequestMapping("chart1.do")
 	// HttpServletResponse : JFreeChart를 이용하기 위한 매개변수 형. servlet에서 사용하는 일반적 매개변수 형
 	public void createChart1(HttpServletResponse response) {
+		try {
+			//차트 객체 리턴
+			JFreeChart chart = chartService.createChart();
+			// 차트를 png로 export
+			// writeChartAsPNG(출력스트림, 차트객체, 가로, 세로)
+			ChartUtilities.writeChartAsPNG(response.getOutputStream(), chart, 900, 550);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@RequestMapping("chart2.do")
+	public ModelAndView createChart2(HttpServletResponse response) {
+		String message = "";
 		
+		try {
+			// 차트 객체를 리턴
+			JFreeChart chart = chartService.createChart();
+			// pdf 문서 객체
+			Document document = new Document(); // itextpdf
+			// pdf 생성 객체
+			PdfWriter.getInstance(document, new FileOutputStream("e:/test.pdf"));
+			// 문서에 데이터를 넣을 수 있도록 open
+			document.open();
+			// 차트를 itextpdf 라이브러리에서 지원하는 이미지 형식으로 변환
+			// : BufferedImage를 png로 변환한 값을 매개값으로 하여 itextpdf의 이미지 객체를 생성
+			Image png = Image.getInstance(ChartUtilities.encodeAsPNG(chart.createBufferedImage(500, 500)));
+			// pdf 문서에 이미지를 추가
+			document.add(png);
+			// pdf 문서 저장
+			document.close();
+			
+			message = "pdf파일이 생성되었습니다.";
+		} catch (Exception e) {
+			message = "pdf파일 생성 실패.";
+			e.printStackTrace();
+		}
+		return new ModelAndView("chart/jchart02", "message", message); 
 	}
 	
 }
--- spring02/src/main/java/com/example/spring02/service/chart/JFreeChartServiceImpl.java
+++ spring02/src/main/java/com/example/spring02/service/chart/JFreeChartServiceImpl.java
@@ -11,6 +11,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.StandardChartTheme;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
 import org.springframework.stereotype.Service;
 
 import com.example.spring02.model.shop.dao.CartDAO;
@@ -27,31 +28,63 @@ public class JFreeChartServiceImpl implements JFreeChartService {
 	
 	@Override
 	public JFreeChart createChart() {
+		// 장바구니 상품목록 가져옴
 		List<CartDTO> list = cartDao.cartMoney();
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		
+		// 1. 차트에 입력할 데이터 셋 객체 (파이차트가 아닌경우)
+		//DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		// 데이터 셋에 값을 넣음
+		//for(CartDTO dto : list) {
+		//	dataset.setValue(dto.getMoney(), "과일", dto.getProduct_name()); // (값, 제목, 이름)
+		//}
+
+		// 2.파이차트용 데이터 셋 생성
+		DefaultPieDataset dataset = new DefaultPieDataset();
 		for(CartDTO dto : list) {
-			dataset.setValue(dto.getMoney(), "과일", dto.getProduct_name());
+			dataset.setValue(dto.getProduct_name(), dto.getMoney());
 		}
+		
+		// 차트 객체 : 차트를 실제로 그려주는 객체
 		JFreeChart chart = null;
+		// 차트 제목
 		String title = "장바구니 통계";
 		try {
-			chart = ChartFactory.createBarChart(title, "상품명", "금액",
-												dataset, PlotOrientation.VERTICAL, true, true, false);
-			// java.awt.Font;
-			chart.getTitle().setFont(new Font("돋움", Font.BOLD, 15));
-			chart.getLegend().setItemFont(new Font("돋움", Font.PLAIN, 12));
+			// 차트 생성
 			
-			Color color = new Color(0,0,0);
+			// 1. 막대그래프 : createBarChart(제목, x축 라벨, y축 라벨, 데이터셋, 그래프 방향, 범례, 툴팁, url)
+			//chart = ChartFactory.createBarChart(title, "상품명", "금액",
+			//									dataset, PlotOrientation.VERTICAL, true, true, false);
+			
+			// 2. 선 그래프
+			//chart = ChartFactory.createLineChart(title, "상품명", "금액",
+			//									dataset, PlotOrientation.VERTICAL, true, true, false);
+			
+			// 3. 파이차트 : createPieChart(제목, 데이터 셋, 범례, 툴팁, url)
+			chart = ChartFactory.createPieChart(title, dataset, true, true, false);
+			
+		// >> 이하 chart의 style
+			// java.awt.Font; : 한글설정
+			chart.getTitle().setFont(new Font("돋움", Font.BOLD, 15)); // 차트 제목 폰트
+			chart.getLegend().setItemFont(new Font("돋움", Font.PLAIN, 10)); // 범례 폰트
+			
+			// 그 외의 폰트지정과 색상지정을 위한 객체. 아래에 각 경우마다 세부지정.
+			Font font = new Font("돋움", Font.PLAIN, 12);
+			Color color = new Color(0,0,0); // black
 			
 			StandardChartTheme chartTheme = (StandardChartTheme)StandardChartTheme.createJFreeTheme();
 			chartTheme.setExtraLargeFont(font);
 			chartTheme.setLargeFont(font);
 			chartTheme.setRegularFont(font);
+			chartTheme.setSmallFont(font);
+			chartTheme.setAxisLabelPaint(color);
+			chartTheme.setLegendItemPaint(color);
+			chartTheme.setItemLabelPaint(color);
+			// 폰트, 컬러를 차트에 적용
+			chartTheme.apply(chart);
+		// <<
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return null;
+		return chart;
 	}
 
 }
